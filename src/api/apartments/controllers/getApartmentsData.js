dca4919c38cb66da9d4c7ec1c3b8d22ab2ff97e0
AKD <contact.mousom@gmail.com>
2023-11-27 17:54:15 +0600

    Pagination is done for apartment route

--- src/api/apartments/controllers/getApartmentsData.js
+++ src/api/apartments/controllers/getApartmentsData.js
@@ -2,10 +2,15 @@ const apartmentData = require("../../../models/apartments/apartments")
 
 
 const getApartmentsData = async(req,res)=>{
-
-    const cursor =await apartmentData.find()
     
-    res.send(cursor)
+    const page = Number(req.query.page)
+    const limit = Number(req.query.limit)
+    const skip = (page-1)*limit;
+
+
+    const cursor = await apartmentData.find().skip(skip).limit(limit)
+    const total = await apartmentData.countDocuments()
+    res.send({cursor, total})
 
 }
 
