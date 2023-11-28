c597902454b0eea553f8a433293eebec170ca060
AKD <contact.mousom@gmail.com>
2023-11-29 03:06:52 +0600

    Add some route and filter some data for members

--- src/api/users/controllers/allUser.js
+++ src/api/users/controllers/allUser.js
@@ -1,9 +1,9 @@
 const User = require('../../../models/userRegister/userRegister')
-const allUser = async(req,res) =>{
+const allUser = async(req,res, next) =>{
 
-    const users = await User.find()
-    
-    res.status(200).send({users: users})
+    const users = await User.find({'userRole' : 'admin'})
+        
+   return res.status(200).send({users: users})
 
 
 }
